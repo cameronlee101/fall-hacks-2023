d8598a328ac5398edf3acc567da74f40bede166c
apa109 <apa109@sfu.ca>
2023-10-21 14:39:25 -0700

    create prompts file

---
+++ src/app/prompts.tsx
@@ -0,0 +1,6 @@
+export const slayDragon = ["Describe a vivid scene of me slaying a dragon with a sword (100 words or less)",
+                           "Pick up a nearby sword and slay the dragon."]
+export const soothDragon = ["Describe a vivid scene of me offering gold to a dragon which convinces it to stop terrorizing a village (100 words or less)",
+                            "Attempt to sooth the dragon by offering all your gold."]
+export const fleeDragon = ["Describe a vivid scene of me narrowly escaping a village being attacked by a dragon with my life (100 words or less)",
+                           "Flee the scene."]
